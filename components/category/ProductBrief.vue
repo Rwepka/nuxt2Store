5807e5b8f4004859cd46037a5db8a3e8226150bd
rwepka <rwepka@gmail.com>
2023-07-31 22:51:46 +0300

    "commit"

---
+++ components/category/ProductBrief.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wrapper">
+    <nuxt-link :to="`/product/${product.pSlug}`">
+      <p>{{ product.pName }}</p>
+      <img :src="product.image" />
+    </nuxt-link>
+    <p>Цена {{ product.pPrice }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    product: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ components/common/CategoriesList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h2>Список категорий</h2>
+    <div class="wrapper">
+      <div
+        v-for="category in categories"
+        :key="category.cSlug"
+        class="block"
+      >
+        <nuxt-link :to="`/category/${category.cSlug}`">
+          <p>{{ category.cName }}</p>
+          <img :src="category.cImage" />
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    categories: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+<!-- <style lang="scss">
+//   В этом месте поломался sass-loader, дальше без него
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////
+  //   in ./components/common/CategoriesList.vue?vue&type=style&index=0&id=0ef42073&lang=scss&  //
+  // Module build failed (from ./node_modules/sass-loader/dist/cjs.js):                         //
+  // TypeError: this.getOptions is not a function                                               //
+  //     at Object.loader (/home/rwepka/nuxt2Store/node_modules/sass-loader/dist/index.js:19:24)//
+  ////////////////////////////////////////////////////////////////////////////////////////////////
+
+.wrapper {
+display: flex;
+}
+.block {
+display: flex;
+flex-direction: column;
+}
+</style> -->
+<style>
+.wrapper {
+display: flex;
+}
+.block {
+display: flex;
+flex-direction: column;
+}
+</style>
